98610c2ec8d1f36e86ba317e1f9fb9f2ee4fd5bb
Silas Stoffel <silasstofel@gmail.com>
2023-09-23 20:26:04 -0300

    chore: force deploy

--- apps/products/serverless.ts
+++ apps/products/serverless.ts
@@ -1,6 +1,6 @@
 import type { AWS } from '@serverless/typescript'
 import { serverlessSidecar } from '@packages/serverless-sidecar'
-
+// force deploy: 1
 const serverless: AWS = {
     service: 'bookstore-products',
     provider: {
