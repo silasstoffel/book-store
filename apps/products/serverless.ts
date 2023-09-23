4148613f33e52b2f5df3d9d7be9dd47cf2cef591
Silas Stoffel <silasstofel@gmail.com>
2023-09-23 20:54:56 -0300

    fix: addig functions

--- apps/products/serverless.ts
+++ apps/products/serverless.ts
@@ -1,5 +1,7 @@
 import type { AWS } from '@serverless/typescript'
 import { serverlessSidecar } from '@packages/serverless-sidecar'
+import functions from './src/handlers'
+
 // force deploy: 1
 const serverless: AWS = {
     service: 'bookstore-products',
@@ -7,7 +9,8 @@ const serverless: AWS = {
         name: 'aws',
         memorySize: 128,
         environment: {}
-    }
+    },
+    functions
 }
 
 module.exports = serverlessSidecar(serverless)
