96556c5815c9e2d33843a69625cb282c5a90d88c
Silas Stoffel <silasstofel@gmail.com>
2023-11-26 01:00:17 -0300

    refactor: create product usecase

--- apps/products/src/infra/handlers/create-product/handler.ts
+++ apps/products/src/infra/handlers/create-product/handler.ts
@@ -3,14 +3,18 @@ import middy from '@middy/core'
 import { HttpValidatorMiddleware, MongooseConnectionMiddleware } from '@packages/middlewares'
 import { Logger } from '@packages/logger'
 import { createProductSchema } from './schema'
-import getProductModel from "../../database/model/product.model";
+import getProductModel from '../../database/model/product.model';
+import { ProductRepository } from '../../database/repository/product.repository';
+import { CreateProductUseCase } from '../../../use-cases/create/create-product.use-case';
+import { CreateProductInput } from '../../../use-cases/create/create-product.input';
 
 const handler = async (event: APIGatewayEvent, context: Context) => {
     const logger = Logger.build({ context });
     logger.info('Creating product');
-    const model = getProductModel()
     const payload = createProductSchema.parse(JSON.parse(event.body || '{}'))
-    const product = await model.create(payload)
+    const repository = new ProductRepository(getProductModel())
+    const useCase = new CreateProductUseCase(repository)
+    const product = await useCase.execute(payload as CreateProductInput)
     logger.info('Product created', { product: product.id })
 
     return { statusCode: 201, body: JSON.stringify(product) }
