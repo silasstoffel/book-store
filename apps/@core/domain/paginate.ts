1db23b9a108c62e36a3e14156ad04cbe6708213f
Silas Stoffel <silasstofel@gmail.com>
2023-12-16 15:09:57 -0300

    refactor: refactoring pagination builder

--- apps/@core/domain/paginate.ts
+++ apps/@core/domain/paginate.ts
@@ -20,18 +20,19 @@ export const LIMIT_DEFAULT = 12
 
 export const buildPaginateResponse = (args: PaginateArgs, data = []) => {
     const { limit } = { limit: LIMIT_DEFAULT, ...args }
-    if (data.length && args?.endingBefore) {
-        data.pop()
-    }
     const counter = data.length
     const hasMore = counter > limit
+
+    if (hasMore) {
+        data.pop()
+    }
     data = args?.endingBefore ? data.reverse() : data
 
     return {
         hasMore,
         data,
         info: {
-            startingAfter: counter ? data[counter-1].id: null,
+            startingAfter: hasMore ? data[counter-1].id: null,
             endingBefore: counter ? data[0].id : null,
         }
     }
