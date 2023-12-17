f8c82b9354e22a9c4229e93b32525fc7268016d6
Silas Stoffel <silasstofel@gmail.com>
2023-12-16 23:48:15 -0300

    chore: changing source event bridge

--- apps/products/src/infra/handlers/load-event/index.ts
+++ apps/products/src/infra/handlers/load-event/index.ts
@@ -4,31 +4,33 @@ import { Event } from '@packages/events'
 export default {
     handler: `${handlerPath(__dirname)}/handler.main`,
     events: [
-        {
-            eventBridge: {
-                eventBus: {
-                    arn: {
-                        'Fn::Join': [
-                            '',
-                            [
-                              'arn:aws:events',
-                              ':',
-                              '${self:provider.region}',
-                              ':',
-                              { Ref: 'AWS::AccountId' },
-                              ':',
-                              'event-bus/book-store',
-                            ],
-                          ],
-                    }
-                },
-                pattern: {
-                  'detail-type': [
-                    Event.PRODUCT_CREATED,
-                    Event.PRODUCT_UPDATED,
-                    Event.PRODUCT_DELETED],
-                },
-              },
-        }
-    ]
-}
+      {
+        eventBridge: {
+          eventBus: {
+            arn: {
+              'Fn::Join': [
+                '',
+                [
+                  'arn:aws:events',
+                  ':',
+                  '${self:provider.region}',
+                  ':',
+                  { 'Fn::Sub': '${AWS::AccountId}' },
+                  ':',
+                  'event-bus/book-store',
+                ],
+              ],
+            },
+          },
+          pattern: {
+            'detail-type': [
+              Event.PRODUCT_CREATED,
+              Event.PRODUCT_UPDATED,
+              Event.PRODUCT_DELETED,
+            ],
+          },
+        },
+      },
+    ],
+  };
+
